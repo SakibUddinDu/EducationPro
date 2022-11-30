3a11278ca9a2fa1802e0b9cae33afc4562e78ba6
Mainuddinhasansakib <sakibuddin831@gmail.com>
2022-11-30 15:29:35 +0600

    navbar updated

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btn = document.getElementById('menu-btn')
 const mobileMenu = document.getElementById('menu')
 
 btn.addEventListener('click', () => {
-//   btn.classList.toggle('open')
+  btn.classList.toggle('open')
   mobileMenu.classList.toggle('flex')
   mobileMenu.classList.toggle('hidden')
 })
