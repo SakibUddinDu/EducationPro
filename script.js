aaf4eebf8179563d50496c02b74c71882564d81b
Mainuddinhasansakib <sakibuddin831@gmail.com>
2022-11-30 15:21:38 +0600

    navbar included

---
+++ script.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('menu-btn')
+const mobileMenu = document.getElementById('menu')
+
+btn.addEventListener('click', () => {
+//   btn.classList.toggle('open')
+  mobileMenu.classList.toggle('flex')
+  mobileMenu.classList.toggle('hidden')
+})
